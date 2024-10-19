b475276ff9bfd6c394c941d70cbca26d635e5b96
Pilar <pilarcosta123@gmail.com>
2024-10-18 21:23:40 -0300

    cambios

--- controladores/controladorSede.php
+++ controladores/controladorSede.php
@@ -14,14 +14,14 @@ class controladorSede {
 
     public function sedes() {
         $sedes = $this->modelo->obtenerCategorias();  // Obtener las sedes desde el modelo
-        include 'C:/xampp/htdocs/web2/tpe2/voluntariado/vistas/categorias.phtml';  
+        include './vistas/categorias.phtml';  
     }
 
     // Función para mostrar los ítems (voluntarios) de una categoría (sede)
     public function mostrarVoluntarios($id_sede) {
         $voluntarios = $this->modelo->obtenerVoluntariosPorCategoria($id_sede); 
         if ($voluntarios) {
-            include 'C:/xampp/htdocs/web2/tpe2/voluntariado/vistas/voluntario.por.categoria.phtml'; 
+            include './vistas/voluntario.por.categoria.phtml'; 
         } else {
             echo "No se encontraron voluntarios para la sede con ID:" . $id_sede;
         }
